191bbbf32543d921a69362bcf9b3c96cf2423fab
Brandon Dusch <brandondusch@gmail.com>
2020-04-07 00:04:50 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,9 +48,9 @@ class InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
-def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-  DB[:conn].execute(sql, name)
+def find_by_name
+  sql = "SELECT * FROM #{self.class.table_name} WHERE name = ?"
+  DB[:conn].execute(sql, self.name)
 end
 
 end
